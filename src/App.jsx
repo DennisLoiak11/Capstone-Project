8a3cff8a3dc1d0189a80b99a7c69d73943835086
DennisLoiak11 <dennisloiak11@gmail.com>
2023-07-03 18:47:59 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/MyNavbar';
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
